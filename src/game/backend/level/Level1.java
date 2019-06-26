11d087a46ed34d381c02fe7e78dc82de1105d234
Tobias Brandy <tobiasbrandy@hotmail.com>
2019-06-26 14:36:44 -0300

    Primeros testers

--- src/game/backend/level/Level1.java
+++ src/game/backend/level/Level1.java
@@ -4,6 +4,7 @@ import game.backend.GameState;
 import game.backend.Grid;
 import game.backend.cell.CandyGeneratorCell;
 import game.backend.cell.Cell;
+import game.backend.element.Candy;
 import game.backend.element.Wall;
 
 public class Level1 extends Grid {
---
+++ src/test/BasicTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import game.backend.CandyGame;
+import game.backend.level.Level1;
+import game.frontend.CandyFrame;
+import game.frontend.GameApp;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BasicTest extends Application{
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        CandyGame game = new CandyGame(Level1Test.class);
+        CandyFrame frame = new CandyFrame(game);
+        Scene scene = new Scene(frame);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
