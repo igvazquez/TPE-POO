4dff08e048b8324cdd98207a146fef283db9d0f3
Tobias Brandy <tobiasbrandy@hotmail.com>
2019-06-26 11:30:52 -0300

    Prueba de commit

--- src/game/backend/level/Level1.java
+++ src/game/backend/level/Level1.java
@@ -9,7 +9,7 @@ import game.backend.element.Wall;
 public class Level1 extends Grid {
 	
 	private static int REQUIRED_SCORE = 5000; 
-	private static int MAX_MOVES = 20; 
+	private static int MAX_MOVES = 20;
 	
 	private Cell wallCell;
 	private Cell candyGenCell;
